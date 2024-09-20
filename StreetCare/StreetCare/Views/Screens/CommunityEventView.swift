023f85a768678efbb8607a73b1f05acc762a0acf
marianbme <marian@brightmindenrichment.org>
2024-09-20 17:17:36 -0500

    filter_change

--- StreetCare/StreetCare/Views/Screens/CommunityEventView.swift
+++ StreetCare/StreetCare/Views/Screens/CommunityEventView.swift
@@ -9,116 +9,111 @@ import Foundation
 import SwiftUI
 import FirebaseAuth
 
+
 enum EventType: Int{
     case future
     case past
     case helpinghands
 }
-enum DateFilter: Int {
-    case next7Days = 7
-    case next30Days = 30
-    case next60Days = 60
+
+enum DateRangeFilter {
+    case next7Days
+    case next30Days
+    case next60Days
 }
 
-struct DateFilterPicker: View {
-    @Binding var selectedFilter: DateFilter
-
-    var body: some View {
-        Picker("Select Filter", selection: $selectedFilter) {
-            Text("Next 7 Days").tag(DateFilter.next7Days)
-            Text("Next 30 Days").tag(DateFilter.next30Days)
-            Text("Next 60 Days").tag(DateFilter.next60Days)
-        }
-        .pickerStyle(SegmentedPickerStyle())
-    }
-}
 
 struct CommunityEventView: View {
     
     @State var user: User?
-    @State var currentData = EventData()
+    @State var currentData =  EventData()
     let adapter = EventDataAdapter()
     @StateObject private var viewModel = SearchCommunityModel()
     @State private var isBottomSheetPresented = false
-    @State private var selectedFilter: DateFilter = .next7Days
+    @State private var selectedDateRange: DateRangeFilter = .next7Days
+
+
     let formatter = DateFormatter()
-    var eventType: EventType
+    var eventType : EventType
   
     var body: some View {
         
         VStack {
-            DateFilterPicker(selectedFilter: $selectedFilter)
-                .padding()
+            
+            // Date range picker
+                        Picker("Filter by Date Range", selection: $selectedDateRange) {
+                            Text("Next 7 Days").tag(DateRangeFilter.next7Days)
+                            Text("Next 30 Days").tag(DateRangeFilter.next30Days)
+                            Text("Next 60 Days").tag(DateRangeFilter.next60Days)
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        .padding()
+                        .onChange(of: selectedDateRange) { newValue in
+                            viewModel.filterEventsByDateRange(newValue)
+                        }
+
             
             HStack {
-                Image(systemName: "magnifyingglass")
-                    .padding(.horizontal, 10)
-                    .foregroundColor(.black)
-                
-                TextField("Search...", text: $viewModel.searchText)
-                    .frame(height: 50.0)
+                    Image(systemName: "magnifyingglass").padding(.horizontal, 10)
+                              .foregroundColor(.black)
+                    TextField("Search...", text: $viewModel.searchText).frame(height: 50.0)
+                }.overlay(
+                        RoundedRectangle(cornerRadius: 15.0)
+                            .stroke(Color.black, lineWidth: 1))
+                .padding()
+            if viewModel.filteredData.isEmpty{
+                Text(NSLocalizedString(eventType == .future ? "noFutureEventsAvailable" : eventType == .past ? "noPastEventsAvailable" : "noHelpingRequestsAvailable", comment: "")).fontWeight(.bold).foregroundColor(Color("TextColor"))
             }
-            .overlay(
-                RoundedRectangle(cornerRadius: 15.0)
-                    .stroke(Color.black, lineWidth: 1)
-            )
-            .padding()
-            
-            if viewModel.filteredData.isEmpty {
-                Text(NSLocalizedString(eventType == .future ? "noFutureEventsAvailable" : eventType == .past ? "noPastEventsAvailable" : "noHelpingRequestsAvailable", comment: ""))
-                    .fontWeight(.bold)
-                    .foregroundColor(Color("TextColor"))
-            } else {
-                List {
-                    ForEach(0..<viewModel.filteredData.count, id: \.self) { index in
-                        if let date = viewModel.filteredData[index].keys.first,
-                           let eventObj = viewModel.filteredData[index][date] {
-                            Section(header: SectionHeaderView(date: date)) {
-                                ForEach(eventObj) { event in
-                                    HStack {
-                                        VStack {
-                                            if let date = event.date.0 {
-                                                Text("\(date)")
-                                                    .font(.system(size: 14, weight: .bold))
-                                                    .foregroundColor(Color("TextColor"))
+            if let _ = self.user {
+                if viewModel.filteredData.isEmpty{
+                    Spacer(minLength: 50.0)
+                }else{
+                    List {
+                        ForEach(0..<viewModel.filteredData.count, id: \.self) { index in
+                            if let date = viewModel.filteredData[index].keys.first,
+                               let eventObj = viewModel.filteredData[index][date] {
+                                Section(header: SectionHeaderView(date: date)){
+                                    ForEach(eventObj) { event in
+                                        HStack{
+                                            VStack{
+                                                if let date = event.date.0{
+                                                    Text("\(date)").font(.system(size: 14, weight: .bold))
+                                                        .foregroundColor(Color("TextColor"))
+                                                }
+                                                if let date = event.date.1{
+                                                    Text("\(date)").font(.system(size: 12, weight: .regular))
+                                                        .foregroundColor(Color("TextColor"))
+                                                }
                                             }
-                                            if let date = event.date.1 {
-                                                Text("\(date)")
-                                                    .font(.system(size: 12, weight: .regular))
-                                                    .foregroundColor(Color("TextColor"))
+                                            EventCardView(event: event, eventType: eventType) {
+                                                currentData.date = event.date
+                                                currentData.monthYear = event.monthYear
+                                                currentData.event = event.event
+                                                isBottomSheetPresented.toggle()
                                             }
-                                        }
-                                        EventCardView(event: event, eventType: eventType) {
-                                            currentData.date = event.date
-                                            currentData.monthYear = event.monthYear
-                                            currentData.event = event.event
-                                            isBottomSheetPresented.toggle()
                                         }
                                     }
                                 }
                             }
-                        }
-                    }
-                    .listRowSeparatorTint(.clear, edges: .all)
-                    .listSectionSeparatorTint(.clear, edges: .all)
+                        }.listRowSeparatorTint(.clear, edges: .all)
+                            .listSectionSeparatorTint(.clear, edges: .all)
+                    }.listStyle(PlainListStyle())
+                        .navigationTitle(eventType == .future ? NSLocalizedString("futureEvents", comment: "") : eventType == .past ? NSLocalizedString("pastEvents", comment: "") : NSLocalizedString("helpRequests", comment: ""))
+                    .scrollContentBackground(.hidden)
+                    .background(Color.clear)
+
                 }
-                .listStyle(PlainListStyle())
-                .navigationTitle(eventType == .future ? NSLocalizedString("futureEvents", comment: "") : eventType == .past ? NSLocalizedString("pastEvents", comment: "") : NSLocalizedString("helpRequests", comment: ""))
-                .scrollContentBackground(.hidden)
-                .background(Color.clear)
-            }
-            
-            if user == nil {
+            }  else {
                 Image("CommunityOfThree").padding()
                 Text("Log in to connect with your local community.")
             }
-        }
-        .bottomSheet(isPresented: $isBottomSheetPresented) {
+        }.bottomSheet(isPresented: $isBottomSheetPresented) {
             VStack {
-                EventPopupView(event: currentData, eventType: eventType, delegate: self)
+                EventPopupView(event: currentData,eventType: eventType, delegate: self)
             }
             .frame(maxWidth: .infinity)
             .padding()
+            
         }
         .onAppear {
             if let user = Auth.auth().currentUser {
@@ -126,17 +121,14 @@ struct CommunityEventView: View {
                 adapter.delegate = self
                 adapter.refresh()
             }
-        }
-        .onChange(of: selectedFilter) { newFilter in
-            viewModel.filterEvents(eventType: eventType, filter: newFilter)
-        }
-        .onDisappear {
+        }.onDisappear {
             isBottomSheetPresented = false
         }
     }
 }
 
-extension CommunityEventView: EventPopupViewDelegate {
+extension CommunityEventView:EventPopupViewDelegate{
+  
     func close() {
         isBottomSheetPresented.toggle()
     }
@@ -144,20 +136,39 @@ extension CommunityEventView: EventPopupViewDelegate {
 
 extension CommunityEventView: EventDataAdapterProtocol {
     func helpRequestDataRefreshed(_ events: [HelpRequest]) {
-        // Implement as needed
     }
     
     func eventDataRefreshed(_ events: [Event]) {
-        viewModel.events = events.filter { $0.eventDate != nil }
-        viewModel.filterEvents(eventType: eventType, filter: selectedFilter)
+        viewModel.events = events.filter({ event in
+            return event.eventDate != nil
+        })
+        viewModel.filterEvents(eventType: eventType)
     }
 }
 
-extension Sequence where Element: Hashable {
-    func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U:[Iterator.Element]] {
-        return Dictionary(grouping: self, by: key)
+
+struct CommunityEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunityEventView(eventType: .future)
     }
 }
+
+
+extension Sequence where Element: Identifiable {
+    func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U:[Iterator.Element]] {
+        return Dictionary.init(grouping: self, by: key)
+    }
+}
+extension Sequence where Element: Hashable {
+    func uniqued() -> [Element] {
+        var set = Set<Element>()
+        return filter { set.insert($0).inserted }
+    }
+    func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U:[Iterator.Element]] {
+        return Dictionary.init(grouping: self, by: key)
+    }
+}
+
 
 struct EventCardView: View {
     var event: EventData
@@ -238,7 +249,7 @@ class SearchCommunityModel: ObservableObject {
     @Published var sectionsAndItems: [[String: [EventData]]] = [[String: [EventData]]]()
     @Published var tempSectionsAndItems: [[String: [EventData]]] = [[String: [EventData]]]()
     @Published var events = [Event]()
-    
+
     var filteredData: [[String: [EventData]]] {
         if searchText.isEmpty {
             return tempSectionsAndItems
@@ -251,26 +262,41 @@ class SearchCommunityModel: ObservableObject {
         }
     }
     
-    func filterEvents(eventType: EventType, filter: DateFilter?) {
+    // New method to filter events based on the date range filter
+        func filterEventsByDateRange(_ dateRange: DateRangeFilter) {
+            guard !events.isEmpty else { return }
+            
+            let calendar = Calendar.current
+            let today = Date()
+            
+            let filteredEvents: [Event] = events.filter { event in
+                guard let eventDate = event.eventDate else { return false }
+                let eventDateObj = convertDateToEst(date: "\(eventDate)")
+                
+                switch dateRange {
+                case .next7Days:
+                    return calendar.isDate(eventDateObj, inSameDayAs: today) || (eventDateObj > today && eventDateObj <= calendar.date(byAdding: .day, value: 7, to: today)!)
+                case .next30Days:
+                    return calendar.isDate(eventDateObj, inSameDayAs: today) || (eventDateObj > today && eventDateObj <= calendar.date(byAdding: .day, value: 30, to: today)!)
+                case .next60Days:
+                    return calendar.isDate(eventDateObj, inSameDayAs: today) || (eventDateObj > today && eventDateObj <= calendar.date(byAdding: .day, value: 60, to: today)!)
+                }
+            }
+            
+            filterAndSortEvents(eventType: .future, dateComparison: { $0 > $1 }, dayComparison: { $0 < $1 }, monthComparison: { $0 < $1 })
+        }
+
+    func filterEvents(eventType: EventType) {
         if events.isEmpty { return }
-        
-        // Call the filtering function with the correct date range
         if eventType == .future {
-            filterAndSortEvents(eventType: .future, dateComparison: { $0 > $1 }, dayComparison: { $0 < $1 }, monthComparison: { $0 < $1 }, filter: filter)
+            filterAndSortEvents(eventType: .future, dateComparison: { $0 > $1 }, dayComparison: { $0 < $1 }, monthComparison: { $0 < $1 })
         } else if eventType == .past {
-            filterAndSortEvents(eventType: .past, dateComparison: { $0 < $1 }, dayComparison: { $0 > $1 }, monthComparison: { $0 > $1 }, filter: filter)
+            filterAndSortEvents(eventType: .past, dateComparison: { $0 < $1 }, dayComparison: { $0 > $1 }, monthComparison: { $0 > $1 })
         }
     }
-    
-    private func filterAndSortEvents(eventType: EventType, dateComparison: @escaping (Date, Date) -> Bool, dayComparison: @escaping (Int, Int) -> Bool, monthComparison: @escaping (Date, Date) -> Bool, filter: DateFilter?) {
+
+    private func filterAndSortEvents(eventType: EventType, dateComparison: @escaping (Date, Date) -> Bool, dayComparison: @escaping (Int, Int) -> Bool, monthComparison: @escaping (Date, Date) -> Bool) {
         var result = [EventData]()
-        
-        // Get the current date and calculate the filter's end date
-        let today = Date()
-        var endDate: Date? = nil
-        if let filter = filter {
-            endDate = Calendar.current.date(byAdding: .day, value: filter.rawValue, to: today)
-        }
         
         let filteredArray = self.events.sorted { firstEvent, secondEvent in
             return dateComparison(firstEvent.eventDate!, secondEvent.eventDate!)
@@ -279,21 +305,18 @@ class SearchCommunityModel: ObservableObject {
         for each in filteredArray {
             if let dateValue = each.eventDate {
                 let dateObj = convertDateToEst(date: "\(dateValue)")
-                // Check if the event falls within the selected filter range
-                if dateComparison(dateObj, today), let endDate = endDate {
-                    if dateObj <= endDate {
-                        let data = EventData()
-                        data.monthYear = formatDateString("\(dateObj)")
-                        data.date.0 = formatDateString("\(dateObj)", format: "dd")
-                        data.date.1 = formatDateString("\(dateObj)", format: "EEE").uppercased()
-                        data.date.2 = formatDateString("\(dateObj)", format: "hh:mm a")
-                        data.event = each
-                        result.append(data)
-                    }
+                if dateComparison(dateObj, Date()) {
+                    let data = EventData()
+                    data.monthYear = formatDateString("\(dateObj)")
+                    data.date.0 = formatDateString("\(dateObj)", format: "dd")
+                    data.date.1 = formatDateString("\(dateObj)", format: "EEE").uppercased()
+                    data.date.2 = formatDateString("\(dateObj)", format: "hh:mm a")
+                    data.event = each
+                    result.append(data)
                 }
             }
         }
-        
+            
         let groupedEvents = result.group(by: { $0.monthYear })
         let sortedEventsByMonth = groupedEvents.sorted { object1, object2 in
             return monthComparison(convertDate(from: object1.key)!, convertDate(from: object2.key)!)
