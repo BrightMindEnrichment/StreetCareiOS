9d0e2b32a689fc195a727b37dfea9389d990dcfc
Kevin Phillips <phillipskevin386@gmail.com>
2024-10-23 23:13:15 -0400

    add EventEnums.swift with project.pbxproj binary mapping file added Marian's code from filters PR

--- StreetCare/StreetCare/Views/Screens/CommunityEventView.swift
+++ StreetCare/StreetCare/Views/Screens/CommunityEventView.swift
@@ -9,22 +9,6 @@ import Foundation
 import SwiftUI
 import FirebaseAuth
 
-enum EventType: Int {
-    case future
-    case past
-    case helpinghands
-}
-
-enum FilterType: String, CaseIterable {
-    case none = "Select.."
-    case next7Days = "Next 7 Days"
-    case next30Days = "Next 30 Days"
-    case next60Days = "Next 60 Days"
-    case next90Days = "Next 90 Days"
-    case otherUpcoming = "Other Upcoming Events"
-    case reset = "Reset"
-}
-
 struct CommunityEventView: View {
     
     @State var user: User?
@@ -41,18 +25,14 @@ struct CommunityEventView: View {
         
         VStack {
             HStack {
+                // Search Bar on the left
                 Image(systemName: "magnifyingglass")
                     .padding(.horizontal, 10)
                     .foregroundColor(.black)
-                TextField("Search...", text: $viewModel.searchText).frame(height: 50.0)
-            }.overlay(
-                RoundedRectangle(cornerRadius: 15.0)
-                    .stroke(Color.black, lineWidth: 1))
-            .padding()
-            
-            // Filter Menu
-            HStack {
-                Spacer()
+                TextField("Search...", text: $viewModel.searchText)
+                    .frame(height: 50.0)
+                
+               /* // Filter Menu on the right, inside the search bar
                 Menu {
                     ForEach(FilterType.allCases, id: \.self) { filter in
                         Button(action: {
@@ -64,15 +44,58 @@ struct CommunityEventView: View {
                     }
                 } label: {
                     HStack {
+                        // Change icon color when a filter is active
                         Image(systemName: "line.horizontal.3.decrease.circle")
+                            .foregroundColor(selectedFilter == .none ? .black : .red) // Red if filter is active
+                        // Show filter name if a filter is selected, otherwise show "Filter"
+                        Text(selectedFilter == .none ? "Filter" : selectedFilter.rawValue)
+                    }
+                    .padding(.horizontal, 10)
+                    .foregroundColor(.black)
+                }*/
+                Menu {
+                    ForEach(FilterType.allCases, id: \.self) { filter in
+                        Button(action: {
+                            selectedFilter = filter
+                            applyFilter(filterType: selectedFilter)
+                        }) {
+                            HStack {
+                                Text(filter.rawValue)
+                                Spacer()
+                                // Checkmark if the filter is selected
+                                if selectedFilter == filter {
+                                    Image(systemName: "checkmark")
+                                        .foregroundColor(.blue) // You can change the color if needed
+                                }
+                            }
+                        }
+                    }
+                } label: {
+                    HStack {
+                        ZStack(alignment: .topTrailing) {
+                            // Main Filter icon
+                            Image(systemName: "line.horizontal.3.decrease.circle")
+                                .foregroundColor(.black)
+                            
+                            // Red dot indicator when a filter is active
+                            if selectedFilter != .none {
+                                Circle()
+                                    .fill(Color.red)
+                                    .frame(width: 8, height: 8) // Adjust size as needed
+                                    .offset(x: 10, y: -5) // Position the red dot
+                            }
+                        }
                         Text("Filter")
                     }
-                    .padding(.horizontal)
+                    .padding(.horizontal, 10)
                     .foregroundColor(.black)
                 }
-                Spacer()
             }
-            .padding(.bottom, 10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 15.0)
+                    .stroke(Color.black, lineWidth: 1)
+            )
+            .padding()
             
             if viewModel.filteredData.isEmpty {
                 Text(NSLocalizedString(eventType == .future ? "noFutureEventsAvailable" : eventType == .past ? "noPastEventsAvailable" : "noHelpingRequestsAvailable", comment: ""))
@@ -148,7 +171,12 @@ struct CommunityEventView: View {
     
     // Method to apply the filter based on user selection
     private func applyFilter(filterType: FilterType) {
-        viewModel.filterByDate(filterType: filterType)
+        // Update the selected filter and apply the changes
+        selectedFilter = filterType
+        viewModel.filterByDate(filterType: selectedFilter)
+
+        // Trigger a UI refresh by updating the published filtered data in viewModel
+        viewModel.objectWillChange.send()  // This will notify the view to update
     }
 }
 
@@ -261,61 +289,110 @@ class SearchCommunityModel: ObservableObject {
         }
     }
     
-        func filterByDate(filterType: FilterType) {
-            let calendar = Calendar.current
-            var filteredEvents = events
+    func filterByDate(filterType: FilterType) {
+        let calendar = Calendar.current
+        let now = Date()
 
-            switch filterType {
-            case .next7Days:
-                filteredEvents = events.filter { event in
-                    if let eventDate = event.eventDate {
-                        return eventDate > Date() && calendar.isDate(eventDate, inRangeOfDays: 7)
-                    }
-                    return false
+        switch filterType {
+        case .next7Days:
+            events = events.filter { event in
+                if let eventDate = event.eventDate {
+                    return calendar.isDate(eventDate, inRangeOfDays: 7)
                 }
-
-            case .next30Days:
-                filteredEvents = events.filter { event in
-                    if let eventDate = event.eventDate {
-                        return calendar.isDate(eventDate, inRangeOfDays: 30)
-                    }
-                    return false
-                }
-            case .next60Days:
-                filteredEvents = events.filter { event in
-                    if let eventDate = event.eventDate {
-                        return calendar.isDate(eventDate, inRangeOfDays: 60)
-                    }
-                    return false
-                }
-            case .next90Days:
-                filteredEvents = events.filter { event in
-                    if let eventDate = event.eventDate {
-                        return calendar.isDate(eventDate, inRangeOfDays: 90)
-                    }
-                    return false
-                }
-            case .otherUpcoming:
-                filteredEvents = events.filter { event in
-                    if let eventDate = event.eventDate {
-                        return calendar.isDate(eventDate, after: Date().addingTimeInterval(90 * 24 * 60 * 60))
-                    }
-                    return false
-                }
-            case .reset:
-                filteredEvents = events
-            case .none:
-                break
+                return false
             }
 
+        case .next30Days:
+            events = events.filter { event in
+                if let eventDate = event.eventDate {
+                    return calendar.isDate(eventDate, inRangeOfDays: 30)
+                }
+                return false
+            }
+
+        case .next60Days:
+            events = events.filter { event in
+                if let eventDate = event.eventDate {
+                    return calendar.isDate(eventDate, inRangeOfDays: 60)
+                }
+                return false
+            }
+
+        case .next90Days:
+            events = events.filter { event in
+                if let eventDate = event.eventDate {
+                    return calendar.isDate(eventDate, inRangeOfDays: 90)
+                }
+                return false
+            }
+
+        case .otherUpcoming:
+            events = events.filter { event in
+                if let eventDate = event.eventDate {
+                    return eventDate > now.addingTimeInterval(90 * 24 * 60 * 60) // After 90 days from now
+                }
+                return false
+            }
+
+        case .reset:
+            events = events // Reset to original, unfiltered data
+        case .none:
+            break
+        }
+
             // Update filtered events
-            filterEvents(eventType: .future) // Or based on the current event type
+        filterEvents(eventType: .future) // Or based on the current event type
         }
         
-        func filterEvents(eventType: EventType) {
-            // Your existing filter logic by event type (future, past, etc.)
+    func filterEvents(eventType: EventType) {
+            if events.isEmpty { return }
+            if eventType == .future {
+                filterAndSortEvents(eventType: .future, dateComparison: { $0 > $1 }, dayComparison: { $0 < $1 }, monthComparison: { $0 < $1 })
+            } else if eventType == .past {
+                filterAndSortEvents(eventType: .past, dateComparison: { $0 < $1 }, dayComparison: { $0 > $1 }, monthComparison: { $0 > $1 })
+            }
         }
-}
+    private func filterAndSortEvents(eventType: EventType, dateComparison: @escaping (Date, Date) -> Bool, dayComparison: @escaping (Int, Int) -> Bool, monthComparison: @escaping (Date, Date) -> Bool) {
+            var result = [EventData]()
+            
+            let filteredArray = self.events.sorted { firstEvent, secondEvent in
+                return dateComparison(firstEvent.eventDate!, secondEvent.eventDate!)
+            }
+            
+            for each in filteredArray {
+                if let dateValue = each.eventDate {
+                    let dateObj = convertDateToEst(date: "\(dateValue)")
+                    if dateComparison(dateObj, Date()) {
+                        let data = EventData()
+                        data.monthYear = formatDateString("\(dateObj)")
+                        data.date.0 = formatDateString("\(dateObj)", format: "dd")
+                        data.date.1 = formatDateString("\(dateObj)", format: "EEE").uppercased()
+                        data.date.2 = formatDateString("\(dateObj)", format: "hh:mm a")
+                        data.event = each
+                        result.append(data)
+                    }
+                }
+            }
+            
+            let groupedEvents = result.group(by: { $0.monthYear })
+            let sortedEventsByMonth = groupedEvents.sorted { object1, object2 in
+                return monthComparison(convertDate(from: object1.key)!, convertDate(from: object2.key)!)
+            }
+            let newEventsValuesSorted = sortedEventsByMonth.map { (key, events) -> (String, [EventData]) in
+                        let sortedEvents = events.sorted { event1, event2 in
+                            guard let day1 = event1.date.0, let day2 = event2.date.0,
+                                  let day1Int = Int(day1), let day2Int = Int(day2) else { return false }
+                            return dayComparison(day1Int, day2Int)
+                        }
+                        return (key, sortedEvents)
+                    }
+                    
+                    sectionsAndItems.removeAll()
+                    for (month, events) in newEventsValuesSorted {
+                        sectionsAndItems.append(["\(month)": events])
+                    }
+                    tempSectionsAndItems = sectionsAndItems
+                }}
 
 
 // Helper extension to check date range
@@ -326,5 +403,16 @@ extension Calendar {
 
     func isDate(_ date: Date, after dateToCompare: Date) -> Bool {
         return date > dateToCompare
+    }
+}
+extension Array {
+    // Group the array elements by a given key
+    func group<Key: Hashable>(by key: (Element) -> Key) -> [Key: [Element]] {
+        var groupedDict = [Key: [Element]]()
+        for element in self {
+            let keyValue = key(element)
+            groupedDict[keyValue, default: []].append(element)
+        }
+        return groupedDict
     }
 }
