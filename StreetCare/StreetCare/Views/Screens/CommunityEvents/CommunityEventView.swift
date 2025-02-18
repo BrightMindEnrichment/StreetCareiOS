5a3025e91a0254e6fb00ec619d45ec057cccaf3a
marianbme <marian@brightmindenrichment.org>
2025-02-17 19:08:48 -0600

    Added User type clarity

--- StreetCare/StreetCare/Views/Screens/CommunityEvents/CommunityEventView.swift
+++ StreetCare/StreetCare/Views/Screens/CommunityEvents/CommunityEventView.swift
@@ -73,7 +73,7 @@ struct CommunityEventView: View {
                     .stroke(Color.black, lineWidth: 1)
             )
             .padding()
-            
+            LegendView()
             if viewModel.filteredData.isEmpty {
                 Text(NSLocalizedString(eventType == .future ? "noFutureEventsAvailable" : eventType == .past ? "noPastEventsAvailable" : "noHelpingRequestsAvailable", comment: ""))
                     .fontWeight(.bold)
@@ -229,5 +229,39 @@ struct SectionHeaderView: View {
                 .foregroundColor(Color("TextColor"))
         }
         .padding(EdgeInsets(top: 0.0, leading: 0.0, bottom: 5.0, trailing: 0.0))
+    }
+}
+
+
+struct LegendView: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            LegendItem(icon: "checkmark.circle.fill", color: .green, text: "Chapter Leader")
+            LegendItem(icon: "checkmark.circle.fill", color: Color.blue.opacity(0.7), text: "Street Care Hub Leader")
+            LegendItem(icon: "checkmark.circle.fill", color: .purple, text: "Chapter Member")
+            LegendItem(icon: "checkmark.circle.fill", color: .yellow, text: "Account Holder")
+        }
+        .padding(8)
+        .cornerRadius(8)
+        .padding(.horizontal)
+        .lineLimit(3)
+    }
+}
+
+struct LegendItem: View {
+    var icon: String
+    var color: Color
+    var text: String
+
+    var body: some View {
+        HStack(spacing: 4) {
+            Image(systemName: icon)
+                .foregroundColor(color)
+                .background(Circle().fill(Color.white).frame(width: 18, height: 18))
+                .frame(width: 20, height: 20)
+            Text(text)
+                .font(.system(size: 12))
+                .foregroundColor(.black)
+        }
     }
 }
