45adf9c01bcac7a084641e40538d30ece6703d08
Oohasripriya Nandipati <oohasripriya.nandipati-1@ou.edu>
2025-04-22 18:33:54 -0500

    Recent Changes

--- StreetCare/StreetCare/Views/Screens/Visit Log/VisitImpactView.swift
+++ StreetCare/StreetCare/Views/Screens/Visit Log/VisitImpactView.swift
@@ -1,51 +1,5 @@
-//
-//  VisitImpactView.swift
-//  StreetCare
-//
-//  Created by Michael on 5/1/23.
-//
-
 import SwiftUI
 import FirebaseAuth
-
-struct CustomHelpAlert: View {
-    let onOK: () -> Void
-
-    var body: some View {
-        VStack(spacing: 20) {
-            
-            Text(LocalizedStringKey("providedHelpTitle"), comment: "")
-                .font(.custom("Poppins-SemiBold", size: 19))
-                .multilineTextAlignment(.center)
-            Text(LocalizedStringKey("providedHelpMessage"), comment: "")
-                .font(.custom("Poppins-Light", size: 13))
-                .multilineTextAlignment(.center)
-                .fixedSize(horizontal: false, vertical: true)
-//            Divider()
-//                   .frame(height: 1)
-//                   .frame(maxWidth: .infinity)
-//                   .background(Color.gray.opacity(0.3))
-//                   .padding(.top, -10)
-            
-            Button(action: onOK) {
-                Text("OK")
-                    .font(.custom("Poppins-Regular", size: 17))
-                    .foregroundColor(Color(red:   0/255,
-                                           green: 122/255,
-                                           blue: 255/255))
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 12)
-                    .cornerRadius(8)
-            }
-        }
-        .padding(24)
-        .background(Color(.systemBackground))
-        .cornerRadius(16)
-        .shadow(radius: 10)
-        .frame(maxWidth: 300, maxHeight: 310)  // fixed maximum width
-    }
-}
-
 
 struct VisitImpactView: View {
     
@@ -59,64 +13,67 @@ struct VisitImpactView: View {
     @State var itemsDonated = 0
     
     @State var showActionSheet = false
-    
     @State var isLoading = false
     @State var isNavigationActive = false
     @State var showLoginMessage = false
+    @State var showAlert = false
     @State var user: User?
     @Binding var selection: Int
-    @State private var showProvidedHelpAlert = false
 
-    
     var body: some View {
-        ZStack {
-            NavigationStack {
-                VStack {
-                    
-                    Text("VISIT LOG").font(.system(size: 18)).padding()
-                    ImpactView(peopleHelped: peopleHelped, outreaches: outreaches, itemsDonated: itemsDonated)
-                    
-                    Button(action: {
-                        if user != nil {
-                            showProvidedHelpAlert = true
-                            //isNavigationActive = true
-                        } else {
-                            showLoginMessage = true
-                        }
-                    }) {
-                        ZStack {
-                            NavLinkButton(title: "Add new +", width: 120.0, height: 30.0)
-                        }
+        NavigationStack {
+            VStack {
+                Text("VISIT LOG").font(.system(size: 18)).padding()
+                ImpactView(peopleHelped: peopleHelped, outreaches: outreaches, itemsDonated: itemsDonated)
+                
+                Button(action: {
+                    if user != nil {
+                        showAlert = true
+                    } else {
+                        showLoginMessage = true
                     }
-                    .alert(NSLocalizedString("loginRequiredTitle", comment: "")
-                           , isPresented: $showLoginMessage) {
-                        Button("OK", role: .cancel) {
-                            selection = 3
-                        }
-                        Button("Cancel") { }
-                        
-                    } message: {
-                        Text(NSLocalizedString("loginRequiredMessage", comment: ""))
+                }) {
+                    ZStack {
+                        NavLinkButton(title: "Add new +", width: 120.0, height: 30.0)
                     }
-//                    .alert("I provided help!", isPresented: $showProvidedHelpAlert) {
-//                        Button("OK") {
-//                            isNavigationActive = true
-//                        }
-//                    } message: {
-//                        Text("Please fill out this form each time you perform an outreach.This helps you track your contributions and allows StreetCare to bring more support and services to help the community!")
-//                    }
-                    
-                    .navigationDestination(isPresented: $isNavigationActive) {
-                        VisitLogEntry()
+                }
+                .alert(NSLocalizedString("loginRequiredTitle", comment: ""), isPresented: $showLoginMessage) {
+                    Button("OK", role: .cancel) {
+                        selection = 3
                     }
-                    Spacer(minLength: 10.0)
-                    Divider().frame(maxWidth: UIScreen.main.bounds.width - 50 ,minHeight: 0.5)
-                        .background(Color.black)
-                    Spacer(minLength: 10.0)
-                    Text("HISTORY").font(.system(size: 16)).bold()
+                    Button("Cancel", role: .destructive) { }
+                } message: {
+                    Text(NSLocalizedString("loginRequiredMessage", comment: ""))
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text(NSLocalizedString("providedHelpTitle", comment: "")),
+                        message: Text(NSLocalizedString("providedHelpMessage", comment: "")),
+                        dismissButton: .default(Text("OK"), action: {
+                            isNavigationActive = true
+                        })
+                    )
+                }
+                .navigationDestination(isPresented: $isNavigationActive) {
+                    VisitLogEntry()
+                }
+
+                //Spacer(minLength: 10.0)
+                Divider().frame(maxWidth: UIScreen.main.bounds.width - 50 ,minHeight: 0.5)
+                    .background(Color.black)
+                //Spacer(minLength: 10.0)
+
+                Text("HISTORY").font(.system(size: 16)).bold()
+                
+                if history.isEmpty {
+                    Text("You have no logged history.")
+                        .font(.custom("Poppins-Light", size: 14))
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 8)
+                } else {
                     List(history) { item in
                         HStack(spacing: 0) {
-                            // ListConnectorDecorationView()
                             VStack {
                                 HStack {
                                     Text("\(item.whereVisit)").font(.system(size: 15.0)).bold()
@@ -128,105 +85,62 @@ struct VisitImpactView: View {
                                     NavigationLink {
                                         VisitLogView(log: item)
                                     } label: {
-                                        NavLinkButton(title:"Details", width: 80.0,secondaryButton: false,noBorder: true, rightArrowNeeded: false,color: .black).frame(maxWidth: .infinity, alignment: .trailing)
+                                        NavLinkButton(title:"Details", width: 80.0, secondaryButton: false, noBorder: true, rightArrowNeeded: false, color: .black)
+                                            .frame(maxWidth: .infinity, alignment: .trailing)
                                     }
                                 }
                                 Divider().frame(maxWidth: UIScreen.main.bounds.width - 50 ,minHeight: 0.5)
                                     .background(Color.gray.opacity(0.4))
                             }
-                        }.listRowSeparatorTint(.clear, edges: .all)
-                            .listSectionSeparatorTint(.clear, edges: .all)
-                    }.listStyle(PlainListStyle())
-                        .scrollContentBackground(.hidden)
-                        .background(Color.clear)
+                        }
+                        .listRowSeparatorTint(.clear, edges: .all)
+                        .listSectionSeparatorTint(.clear, edges: .all)
+                    }
+                    .listStyle(PlainListStyle())
+                    .scrollContentBackground(.hidden)
+                    .background(Color.clear)
                 }
-                .loadingAnimation(isLoading: isLoading)
-                .onAppear {
-                    print("Impact view onAppear")
-                    adapter.delegate = self
-                    Auth.auth().addStateDidChangeListener { auth, currentUser in
-                        /*
-                         This listener detects changes in authentication state.
-                         - If the user logs in, `currentUser` is updated.
-                         - If the user logs out, `currentUser` becomes nil.
-                         */
-                        self.user = currentUser  // Update the user state
-                    }
-                    
-                    // not sure why I need to do this, the refresh method
-                    // checks for no user and if so calls
-                    // delegate function
-                    // but the loading animation never goes away
-                    // despite the state flagging changing to false
-                    if Auth.auth().currentUser != nil {
-                        adapter.refresh()
-                        self.isLoading = true
-                    }
-                    else {
-                        adapter.resetLogs()
-                        history = [VisitLog]()
-                        peopleHelped = 0
-                        outreaches = 0
-                        itemsDonated = 0
-                    }
-                }
+
+                Spacer()
             }
-            if showProvidedHelpAlert {
-                Color.black.opacity(0.4)
-                    .ignoresSafeArea()
-                CustomHelpAlert {
-                    showProvidedHelpAlert   = false
-                    isNavigationActive = true
+            .loadingAnimation(isLoading: isLoading)
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                print("Impact view onAppear")
+                adapter.delegate = self
+                Auth.auth().addStateDidChangeListener { _, currentUser in
+                    self.user = currentUser
+                }
+                if Auth.auth().currentUser != nil {
+                    adapter.refresh()
+                    self.isLoading = true
+                } else {
+                    adapter.resetLogs()
+                    history = [VisitLog]()
+                    peopleHelped = 0
+                    outreaches = 0
+                    itemsDonated = 0
                 }
             }
         }
     }
     
-    
     private func updateCounts() {
-
         self.outreaches = history.count
-        
-        self.peopleHelped = history.reduce(0, { partialResult, visitLog in
-            partialResult + visitLog.peopleHelped
-        })
-        
-        self.itemsDonated = history.reduce(0, { partialResult, visitLog in
-            
-            var newDonations = 0
-            
-            if visitLog.foodAndDrinks {
-                newDonations += 1
-            }
-
-            if visitLog.clothes {
-                newDonations += 1
-            }
-
-            if visitLog.hygine {
-                newDonations += 1
-            }
-            
-            if visitLog.wellness {
-                newDonations += 1
-            }
-
-            if visitLog.other {
-                newDonations += 1
-            }
-
-            return partialResult + newDonations
-        })
-
-        
+        self.peopleHelped = history.reduce(0) { $0 + $1.peopleHelped }
+        self.itemsDonated = history.reduce(0) { result, visitLog in
+            var count = 0
+            if visitLog.foodAndDrinks { count += 1 }
+            if visitLog.clothes { count += 1 }
+            if visitLog.hygine { count += 1 }
+            if visitLog.wellness { count += 1 }
+            if visitLog.other { count += 1 }
+            return result + count
+        }
     }
-    
-} // end struct
-
-
+}
 
 extension VisitImpactView: VisitLogDataAdapterProtocol {
-    
     func visitLogDataRefreshed(_ logs: [VisitLog]) {
         self.history = logs
         self.updateCounts()
@@ -234,10 +148,8 @@ extension VisitImpactView: VisitLogDataAdapterProtocol {
     }
 }
 
-
 struct VisitImpactView_Previews: PreviewProvider {
     static var previews: some View {
         VisitImpactView(selection: .constant(1))
     }
 }
-
